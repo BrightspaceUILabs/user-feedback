1c4539e3693bc1ee16163d0d5a867b7cc9ae9bcc
Jeff Stefaniuk <jeff.stefaniuk@d2l.com>
2019-11-14 16:24:33 -0600

    Serge (#8)
    * add serge
    
    * actually use it
    
    * chester was right
    
    * .js

---
+++ lang/locales/nl-NL.js
@@ -0,0 +1,12 @@
+/* This file is auto-generated. Do not modify directly. */
+/* eslint quotes: ["error", "double"] */
+
+export const nlNl = {
+	"sendFeedback": "Send Feedback",
+	"dontWantToGiveFeedback": "I don't want to give feedback",
+	"cancel": "Cancel",
+	"close": "Close",
+	"submittedTitle": "Thank You!",
+	"errorSubmitting": "There was an error submitting your feedback, please try again later",
+	"defaultTitle": "Send us Product Feedback"
+};
